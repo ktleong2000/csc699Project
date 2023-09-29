b14946fbedf1e637d214f93bc475b0c9cc82169c
ktleong2000 <brendanleong105@gmail.com>
2023-09-28 21:11:10 -0700

    Added movement but turning with animator is WIP

--- Quick Death MOBA/Assets/Scripts/Core/S_Player/S_PlayerMovement.cs
+++ Quick Death MOBA/Assets/Scripts/Core/S_Player/S_PlayerMovement.cs
@@ -10,18 +10,22 @@ public class S_PlayerMovement : NetworkBehaviour
     [SerializeField] private S_InputReader s_inputReader;
     [SerializeField] private Transform bodyTransform;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
 
     [Header ("Settings")]
     [SerializeField] private float movementSpeed = 4f;
     [SerializeField] private float turningRate = 30f;
 
+
     private UnityEngine.Vector2 previousMovementInput;
+    private bool isRotating = false;
     public override void OnNetworkSpawn()
     {
         if(!IsOwner){
             return;
         }
 
+        animator = GetComponent<Animator>();
         s_inputReader.MoveEvent += HandleMove;
     }
     public override void OnNetworkDespawn()
@@ -43,8 +47,20 @@ public class S_PlayerMovement : NetworkBehaviour
 
         //A and D controls to control the rotation, and the way the hip is facing for
         //movement.
+        
         float xRotation = previousMovementInput.x * turningRate * Time.deltaTime;
-        bodyTransform.Rotate(xRotation, 0f, 0f);
+        
+        if (xRotation != 0)
+        {
+            bodyTransform.Rotate(xRotation, 0f, 0f);
+            isRotating = true; // Set the flag to true while rotating
+        }
+        else
+        {
+            isRotating = false; // Set the flag to false when not rotating
+        }
+        
+
     }
 
     private void FixedUpdate() {
@@ -56,6 +72,21 @@ public class S_PlayerMovement : NetworkBehaviour
         if(!IsOwner){return;}
 
         rb.velocity = (UnityEngine.Vector2)bodyTransform.up * previousMovementInput.y * movementSpeed;
+
+        if (isRotating)
+        {
+            animator.enabled = false; // Disable the animator while rotating
+        }
+        else
+        {
+            animator.enabled = true; // Re-enable the animator when not rotating
+        }
+
+        if(rb.velocity != UnityEngine.Vector2.zero){
+            animator.SetBool("IsMoving", true);
+        }else{
+            animator.SetBool("IsMoving", false);
+        }
     }
 
     private void HandleMove(UnityEngine.Vector2 movementInput){
